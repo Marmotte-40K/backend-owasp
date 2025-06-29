4d29e8499e4b9fa5e6cf048bdcb700cebc6907be
Federico Balducci <ott8vv@gmail.com>
2025-06-29 23:59:14 +0200

    feat: Add database connection

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/Marmotte-40K/backend-owasp/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+var pool *pgxpool.Pool
+
+func init() {
+	var err error
+	pool, err = pgxpool.New(context.Background(), fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+	}
+
+	if err := pool.Ping(context.Background()); err != nil {
+		log.Fatal("Unable to ping database:", err)
+	}
+
+	fmt.Println("Connected to PostgreSQL database!")
+}
+
 func main() {
+
 	router := gin.Default()
+
+	defer pool.Close()
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
